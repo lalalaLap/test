d38bbf2c841b88095212906d97e408b8bc6cdb26
20240392 <pan.pinzheng@huafeng.com>
2024-04-30 18:21:17 +0800

    架构

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.io.*;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("E://workplace//IdeaProject//start//pom.xml");
+        FileOutputStream fileOutputStream = new FileOutputStream("E://workplace//pom.xml");
+
+
+        int b ;
+        while ((b=fileInputStream.read()) != -1){
+            fileOutputStream.write(b);
+        }
+        fileOutputStream.close();
+        fileInputStream.close();
     }
 }
--- src/main/java/test/service/ThreadService.java
+++ src/main/java/test/service/ThreadService.java
@@ -1,4 +1,4 @@
-package service;
+package test.service;
 
 public class ThreadService {
 }
